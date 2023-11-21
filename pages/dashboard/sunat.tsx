a4803a389596886dc8d78e4c092f5678b2d79bc3
franco condori <freconur@gmail.com>
2023-11-21 17:13:21 -0500

    add:testeando nueva funcionalidad de vista de impresoras del dispositivo

--- pages/dashboard/sunat.tsx
+++ pages/dashboard/sunat.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useGlobalContext } from '../../context/GlobalContext'
-
-const initialValueFormUser = {username:"", password:""}
+import {ConectorPluginV3} from '../../plugin-printer'
+const initialValueFormUser = { username: "", password: "" }
 const Sunat = () => {
   const [formUser, setFormUser] = useState<UserApisPeru>(initialValueFormUser)
-  const {loginApisPeruContext} = useGlobalContext() 
-  const formUserOnChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+  const [printers, setPrinters] = useState<any>()
+  const { loginApisPeruContext } = useGlobalContext()
+  const formUserOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormUser({
       ...formUser,
       [e.target.name]: e.target.value
@@ -15,10 +16,38 @@ const Sunat = () => {
     e.preventDefault()
     loginApisPeruContext(formUser)
   }
-  console.log('formUser', formUser)
+  useEffect(() => {
+    getPrinterDevice()
+  },[])
+  const getPrinterDevice = async() => {
+    const printerList = await ConectorPluginV3.obtenerImpresoras()
+    if(printerList) {
+      setPrinters(printerList)
+    }
+  }
+  const sendNewTicket = async () => {
+    const newTicket = new ConectorPluginV3()
+    console.log('formUser', formUser)
+    newTicket
+    .Iniciar()
+    .EstablecerAlineacion(ConectorPluginV3.ALINEACION_CENTRO)
+    .EscribirTexto("test de prueba impresion boleta")
+    .Feed(1)
+    .EscribirTexto("2do mensaje de prueba")
+    .Feed(1)
+    .Iniciar()
+    .Feed(1)
+  
+    const respuesta = await newTicket.imprimirEn()
+
+  }
+  console.log('printers',printers)
   return (
     <div>
-      <form onSubmit={loginApisPeru}>
+
+    <h2 className='p-2 bg-blue-400 '>show printer</h2>
+    
+      {/* <form onSubmit={loginApisPeru}>
         <div>
           <label>usuario</label>
           <input name="username"  onChange={formUserOnChange} type="text" />
@@ -28,7 +57,7 @@ const Sunat = () => {
           <input name="password" onChange={formUserOnChange} type="text" />
         </div>
         <button className='p-3 rounded-sm bg-blue-500 text-white font-semibold'>login</button>
-      </form>
+      </form> */}
     </div>
   )
 }
