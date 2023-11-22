8ce50133e4de1e488514d1282dc928e909e2fdb1
franco condori <freconur@gmail.com>
2023-11-22 18:43:08 -0500

    test

--- pages/dashboard/sunat.tsx
+++ pages/dashboard/sunat.tsx
@@ -32,7 +32,7 @@ const Sunat = () => {
   const URLPlugin = "http://localhost:8000"
   const sendNewTicket = async () => {
     
-    const newTicket = new ConectorPluginV3(URLPlugin)
+    const newTicket = new ConectorPluginV3()
     console.log('formUser', formUser)
     newTicket
     newTicket.Iniciar()
@@ -48,7 +48,7 @@ const Sunat = () => {
     newTicket.Feed(1)
   
     // const respuesta = await newTicket.imprimirEn('KONICA MINOLTA C652SeriesPCL')
-    const respuesta = await newTicket.imprimirEnImpresoraRemota('KONICA MINOLTA C652SeriesPCL',"http://192.168.0.5:8000/imprimir")
+    const respuesta = await newTicket.imprimirEnImpresoraRemota('KONICA MINOLTA C652SeriesPCL',"http://192.168.0.5/imprimir")
     if(respuesta === true) {
       console.log('impresioin correcta')
     }else {
@@ -59,7 +59,7 @@ const Sunat = () => {
   console.log(`${productosDeVenta[0].name.slice(0,5)}             ${productosDeVenta[0].price}`)
   return (
     <div>
-        <p>2</p>
+        <p>3</p>
     <h2 className='p-2 bg-blue-400 '>show printer</h2>
     <select>
     {
