a3303f2168b55e0b7d1860cf8fdd3b7bffb7e1bf
franco condori <freconur@gmail.com>
2023-11-24 14:32:41 -0500

    fix: espaciado de salto y lineas

--- utils/printer.ts
+++ utils/printer.ts
@@ -6,45 +6,48 @@ const productosDeVenta = [
 ]
 // const URLPlugin = "http://localhost:8000"
 
-export const sendNewTicket = async (paymentData:PaymentInfo, products:ProductToCart[] | ProductsFromTicket[] | undefined, timestamp:Date, correlativoTicket:string,userData:User) => {
-console.log('paymentData:',paymentData,'products:',products,'timestamp:',timestamp,'correlativoTicket',correlativoTicket,'userData',userData)
+export const sendNewTicket = async (paymentData: PaymentInfo, products: ProductToCart[] | ProductsFromTicket[] | undefined, timestamp: Date, correlativoTicket: string, userData: User) => {
+  console.log('paymentData:', paymentData, 'products:', products, 'timestamp:', timestamp, 'correlativoTicket', correlativoTicket, 'userData', userData)
   // const newTicket = new ConectorPluginV3(URLPlugin)
   const newTicket = new ConectorPluginV3()
   // console.log('formUser', formUser)
   newTicket.EstablecerAlineacion(ConectorPluginV3.ALINEACION_CENTRO)
-  newTicket.EscribirTexto("Larys S.A.C.")
-  newTicket.Feed(1)
+  newTicket.EscribirTexto("LARYS IMPORTACIONES S.A.C.")
+  newTicket.Feed(0)
   newTicket.EscribirTexto("RUC: 2010020030142")
-  newTicket.Feed(1)
+  newTicket.Feed(0)
   newTicket.EscribirTexto(`NRO.:${correlativoTicket}`)
-  newTicket.Feed(1)
+  newTicket.Feed(0)
   newTicket.Iniciar()
   products?.map(pro => {
-    if(Number(pro.amount) > 1) {
-      newTicket.EscribirTexto(`${pro.code}  ${pro.description?.slice(0,12)}`)
+    if (Number(pro.amount) > 1) {
+      newTicket.EscribirTexto(`${pro.code}  ${(pro.description?.slice(0, 12).toUpperCase())}`)
       newTicket.Feed(1)
-      newTicket.EscribirTexto(`             ${pro.amount} X        ${Number(pro.price).toFixed(2)}        ${(Number(pro.amount)*Number(pro.price)).toFixed(2)}`)
+      newTicket.EscribirTexto(`             ${pro.amount} X        ${Number(pro.price).toFixed(2)}              ${(Number(pro.amount) * Number(pro.price)).toFixed(2)}`)
       newTicket.Feed(1)
-    }else {
-      newTicket.EscribirTexto(`${pro.code}  ${pro.description?.slice(0,12)}        ${pro.price}`)
+    } else {
+      newTicket.EscribirTexto(`${pro.code}  ${(pro.description?.slice(0, 12).toUpperCase())}                ${pro.price}`)
       newTicket.Feed(1)
     }
   })
-  newTicket.EscribirTexto(`OP. GRAVADA:    S/                      ${Number(paymentData.totalAmountToCart * 0.82).toFixed(2)}`)//12//22
+  newTicket.EscribirTexto(`OP. GRAVADA:    S/                         ${Number(paymentData.totalAmountToCart * 0.82).toFixed(2)}`)//12//22
   newTicket.Feed(0)
   newTicket.EscribirTexto(`I.G.V.:         S/                         ${Number(paymentData.totalAmountToCart * 0.18).toFixed(2)}`)//
-  newTicket.Feed(0.2)
-  newTicket.EscribirTexto(`TOTAL A PAGAR:  S/                            ${Number(paymentData.totalAmountToCart).toFixed(2)}`) //14
-  newTicket.Feed(0.3)
-  newTicket.EscribirTexto(`${timestamp.getDate} ${timestamp.getHours}`)
-  newTicket.Feed(0.4)
-  newTicket.EscribirTexto(`Nombre cajero: ${userData.name} ${userData.lastname}`)
+  newTicket.Feed(0)
+  newTicket.EscribirTexto(`TOTAL A PAGAR:  S/                         ${Number(paymentData.totalAmountToCart).toFixed(2)}`) //14
+  newTicket.Feed(0)
+  newTicket.EscribirTexto(`${timestamp.getDate()} ${timestamp.getHours()}`)
+  newTicket.Feed(1)
+  newTicket.Feed(1)
+  newTicket.EscribirTexto(`----------------------------------------------`)
+
+  newTicket.EscribirTexto(`Nombre cajero: ${(userData.name)?.toString().toUpperCase()} ${(userData.lastname)?.toString().toUpperCase()}`)
   newTicket.Feed(0.1)
   newTicket.EscribirTexto(`Cod. cajero: ${userData.identifier}`)
   newTicket.Feed(1)
-  newTicket.EscribirTexto(`Tipo de pago: ${paymentData.cash.amount}`)
+  newTicket.EscribirTexto(`TIPO DE PAGO: ${paymentData.cash.amount > 0 ? "EFECTIVO" : ""}  ${paymentData.yape.amount > 0 ? "YAPE" : ""}`)
   newTicket.Feed(1)
-  newTicket.EscribirTexto(`cash: ${paymentData.cash.amount}  yape:${paymentData.yape.amount} | ${paymentData.yape.operationId}`)
+  newTicket.EscribirTexto(`EFECTIVO: ${paymentData.cash.amount > 0 ? paymentData.cash.amount : 0}  YAPE: ${paymentData.yape.amount > 0 ? paymentData.yape.amount : 0} | N. OP.:${paymentData.yape.operationId}`)
   newTicket.Feed(1)
   newTicket.Corte()
   newTicket.Iniciar()
